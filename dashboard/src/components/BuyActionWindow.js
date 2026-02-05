64cfc17937e25cb0aaa708ea3078e184013bdafa
Krishna <k84497838@gmail.com>
2026-02-05 21:21:20 +0530

    Update API endpoint for buy order submission

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("https://backendzerodaclone.onrender.com", {
+    axios.post("https://backendzerodaclone.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
